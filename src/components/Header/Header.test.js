529aee9fb70d3e00a2ff99fc464cb4db8ea96762
Cristian Rivas <cristianriv@gmail.com>
2021-04-14 22:48:38 -0500

    CREATE Test: Title header

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
+
+test('Render title', () => {
+  render(<Header />);
+  const titleElement = screen.getByText(/gic GAP goe/i);
+  expect(titleElement).toBeInTheDocument();
+});
