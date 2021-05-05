566d076ecc89d406d2a53875effc7e54f19fa983
Cristian Rivas <cristianriv@gmail.com>
2021-05-04 22:10:25 -0500

    ADD Header Title Test -Failure-

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 test('Render title', () => {
-  render(<Header />);
-  const titleElement = screen.getByText(/gic GAP goe/i);
-  expect(titleElement).toBeInTheDocument();
-});
+    render(<Header />);
+    const titleElement = screen.getByText(/gic GAP goe/i);
+    expect(titleElement).toBeInTheDocument();
+  });
