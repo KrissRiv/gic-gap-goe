c3d3ac0b4373b07f8c490983f280f124111de13d
Cristian Rivas <cristianriv@gmail.com>
2021-05-05 02:44:12 -0500

    ADD Test click event -Test Failure-

--- src/components/Atoms/Chip.test.js
+++ src/components/Atoms/Chip.test.js
@@ -6,4 +6,10 @@ describe('Test Chip Component', () => {
     it('Should render Chip', () => {
         shallow(<Chip />);
     });
+    it('Should click event in Chip', () => {
+        const mockCallBack = jest.fn();
+        const chipButton = shallow((<Chip onClick={mockCallBack} />));
+        chipButton.find('button').simulate('click');
+        expect(mockCallBack).toBeCalled();
+    });
 });
