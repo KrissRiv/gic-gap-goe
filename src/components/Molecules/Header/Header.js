64c7cee6972de4e0a58993f955997e2668b3533d
Cristian Rivas <cristianriv@gmail.com>
2021-05-05 02:01:57 -0500

    ADD Logo image -Test Success-

--- src/components/Molecules/Header/Header.js
+++ src/components/Molecules/Header/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import GAPLogo from '../../../Assets/gap-logo.svg';
 import './Header.css';
 
 class Header extends Component {
   render () {
     return (
       <header className="header">
-        <h1>gic GAP goe</h1>
+        <img src={GAPLogo} alt="GAP Logo" className="header__logo" />
+        <h1>gic GAP goe Game</h1>
       </header>
     );
   }
