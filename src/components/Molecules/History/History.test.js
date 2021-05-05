79dd1d84bdb5b3e50ee875470fdf8d82891405b6
Cristian Rivas <cristianriv@gmail.com>
2021-05-05 14:31:25 -0500

    ADD Test for render with props -Test Failure-

--- src/components/Molecules/History/History.test.js
+++ src/components/Molecules/History/History.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import History from "./History";
 
 describe('Test History Component', () => {
     it('Should render History', () => {
         shallow(<History />);
+    });
+    it('allows us to set props', () => {
+        const wrapper = shallow(<History status='Status Prop' />);
+        expect(wrapper.props().status).toEqual('Status Prop');
+        
+        wrapper.setProps({ status: 'Change Prop' });
+        expect(wrapper.props().status).toEqual('Change Prop');
     });
 });
